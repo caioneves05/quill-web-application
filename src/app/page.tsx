2c0540abfc61bb9ab1819ee8d2f605fadc3c0229
Caio Neves <ncaio037@gmail.com>
2024-04-14 18:33:06 -0300

    feat: kinde authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,7 +114,7 @@ export default function Home() {
           <div className="-mt-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
             <Image 
                 src={'/file-upload-preview.jpg'} 
-                width={1364} height={866} 
+                width={1419} height={732} 
                 quality={100}
                 alt="uploading preview"
                 className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
