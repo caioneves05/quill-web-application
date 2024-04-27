c77b1af8ba0fe9a7c1f8b70ae73f3f6d18347b79
Caio Neves <ncaio037@gmail.com>
2024-04-26 23:05:24 -0300

    feat: added database

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,20 +1,23 @@
 import { useSearchParams } from "next/navigation"
 import { useRouter } from "next/router"
 import { trpc } from "../_trpc/client"
+import { Loader2 } from "lucide-react"
 
 const Page = async () => {
-    const router = useRouter()
 
-    const searchParamas = useSearchParams()
+    await trpc.authCallback.query()
 
-    const origin = searchParamas.get('origin')
-
-    const { success } =  await trpc.authCallback.query()
-
-    if(success) {
-        // user is synced to db
-        router.push(origin ? `/${origin}` : `/dashboard`)
-    }
+    return(
+        <div className="w-full mt-24 flex justify-center">
+            <div className="flex flex-col items-center gap-2">
+                <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+                <h3 className="font-semibold text-xl">
+                    Setting up your account
+                </h3>
+                <p>You will be redirected automatically</p>
+            </div>
+        </div>
+    )
 
 }
 
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,19 +1,46 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { publicProcedure, router } from "./trpc"
 import { TRPCError } from "@trpc/server/unstable-core-do-not-import"
+import { db } from "@/database"
+import { useSearchParams } from "next/navigation"
+import { useRouter } from "next/router"
 
 
 export const appRouter = router({
     authCallback: publicProcedure.query(async () => {
+
+        const router = useRouter()
+        const searchParamas = useSearchParams()
+        const origin = searchParamas.get('origin')
+
         const { getUser } = getKindeServerSession()
         
         const user = await getUser()
 
-        if(!user?.id || user.email) throw new TRPCError({ code: 'UNAUTHORIZED' })
+        if(!user?.id || user.email) {
 
-        // check if the user is in the database
+            router.push('/sign-in')
 
-        return { success: true }
+            return
+        } 
+        
+        const dbUser = await db.user.findFirst({
+            where: {
+                id: user.id
+            }
+        })
+
+        if(!dbUser) {
+            await db.user.create({
+                data: {
+                    id: user.id,
+                    email: user.email as string,
+                }
+            })
+        }
+
+        router.push(origin ? `/${origin}` : `/dashboard`)
+    
     })
 })
 
